4740910bd117a152f61a86073a0a0b310b05f622
SparkkkyZzx <miguelspark2007@gmail.com>
2026-06-06 14:27:24 -0400

    atividade 5

---
+++ src/Livro.java
@@ -0,0 +1,19 @@
+public class Livro {
+    String titulo;
+    String isbn;
+
+    public Livro(String titulo, String isbn) {
+           this.titulo = titulo;
+           this.isbn = isbn;
+    }
+   }
+
+
+
+
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String a = "Java";
-        String b = "Java";
-        String c = new String("Java");
-
-        System.out.println(a == b);   //true - porque a e b apontam para o mesmo objeto na pool de strings
-        System.out.println(a == c);   //false - porque c é um novo objeto criado com o operador new, então ele não aponta para o mesmo objeto na pool de strings
-        System.out.println(a.equals(c)); //true - porque o método equals() compara o conteúdo das strings, e tanto a quanto c têm o mesmo conteúdo "Java"
+        Livro l1 = new Livro("Clean Code", "978-0132350884");
+        Livro l2 = new Livro("Clean Code", "978-0132350884");
+        System.out.println(l1 == l2); // ?
+        System.out.println(l1.equals(l2)); // ?
     }
 }
-
-
-// A string Pool é uma area diferente ou especiais que o Java utiliza para armazenar strings literais. Quando uma string é criada usando aspas duplas (""), ela é automaticamente adicionada à string pool. Se outra string com o mesmo conteúdo for criada, em vez de criar um novo objeto, o Java reutiliza a referência para a string existente na pool. Isso economiza memória e melhora o desempenho, pois evita a criação de múltiplos objetos para strings idênticas. No entanto, quando uma string é criada usando o operador new, um novo objeto é criado na heap.
+// L1 E L2 SÃO OBJETOS DIFERENTES, MESMO CONTEUDO, POR ISSO O RESULTADO DO == É FALSE, POIS ELE COMPARA AS REFERENCIAS DE MEMORIA, ENQUANTO O EQUALS COMPARA O CONTEUDO DOS OBJETOS, SE O EQUALS NAO FOR SOBRESCRITO NA CLASSE LIVRO, ELE VAI HERDAR O COMPORTAMENTO DA CLASSE OBJECT, QUE TAMBEM COMPARA AS REFERENCIAS DE MEMORIA, ENTÃO O RESULTADO DO EQUALS TAMBEM VAI SER FALSE.
